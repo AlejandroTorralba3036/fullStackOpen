3cac23c18c27eb4b5b37556b19647a0e41833ad3
Alejandro Torralba <al2153036494@azc.uam.mx>
2025-07-08 15:06:22 -0600

    Add initial .gitignore and pruebas.js for testing repository setup

---
+++ part0/pruebas.js
@@ -0,0 +1,5 @@
+// No presentare los primeros ejercicios de la parte 0
+// Esto es una prueba para probar el repositorio
+
+console.log("Prueba de código en el repositorio");
+console.log("Hola mundo");
